76177ba930869a04a67230f6218f6ecd6a0c1c3d
fayazap <fayazap2@gmail.com>
2022-05-15 10:13:41 +0530

    Email Recieving update

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com';
+$email_from = 'info@universityyy.netlify.app/';
 
 $email_subject = 'New Form Submission';
 
